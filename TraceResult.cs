6cedf5be1d5cb374ed47092fb889576a75797d54
olegbordilovskiy <vanasilvergoldsssk@mail.ru>
2024-02-18 13:19:43 +0300

    Добавьте файлы проекта.

---
+++ TraceResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    internal class TraceResult
+    {
+        public string? MethodName { get; set; }
+        public string? ClassName { get; set; }
+        public string? ExecutionTime { get; set; }
+    }
+}
---
+++ Tracer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer.Interfaces;
+
+namespace Tracer
+{
+    internal class Tracer : ITracer
+    {
+        public void StartTrace() { }
+        public void StopTrace() { }
+    }
+}
